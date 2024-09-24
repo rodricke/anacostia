b1dc679db29ac7b38770c113cd1347bcc14101d1
rodricke <rodrick@ymail.com>
2024-09-24 04:26:17 -0400

    Update Program.cs 5.0

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        string version = "4.0.0";
+        string version = "5.0.0";
         string environment = Environment.GetEnvironmentVariable("RELEASE_ENVIRONMENTNAME") ?? "Unknown";
         Console.WriteLine($"Hello World version {version} from {environment} environment");
     }
